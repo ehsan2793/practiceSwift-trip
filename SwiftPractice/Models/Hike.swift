dfd0f07c452665d0728978fb09b3ac8433551c23
ehsan2793 <ehsan2793@gmail.com>
2024-07-26 16:30:56 -0700

    first commit

---
+++ SwiftPractice/Models/Hike.swift
@@ -0,0 +1,18 @@
+//
+//  Hike.swift
+//  SwiftPractice
+//
+//  Created by Ehsan Rahimi on 7/26/24.
+//
+
+import SwiftUI
+
+struct Hike: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hike()
+}
