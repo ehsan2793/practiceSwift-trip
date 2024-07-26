0f1c0bec344d62e7775c016c93a3f8a7fedd7327
ehsan2793 <ehsan2793@gmail.com>
2024-07-25 21:21:55 -0700

    Initial Commit

---
+++ SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by Ehsan Rahimi on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
