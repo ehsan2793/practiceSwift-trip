88c1a9b6ea297af6a9c1298587303b8092d3ffef
ehsan2793 <ehsan2793@gmail.com>
2024-07-26 19:35:16 -0700

    added little animation

--- SwiftPractice/HikeDetailView.swift
+++ SwiftPractice/HikeDetailView.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct HikeDetailView: View {
     let hike: Hike
+    @State private var zoomed = false
     var body: some View {
         VStack {
             Image(hike.photo)
                 .resizable()
-                .scaledToFit()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
                 .shadow(color: .black, radius: 5, x: 0, y: 5)
                 .padding(.horizontal)
+                .onTapGesture {
+                    withAnimation(.spring(duration: 0.5, bounce: 0.1, blendDuration: 0.5)) {
+                        zoomed.toggle()
+                    }
+                }
 
             Text(hike.name)
                 .font(.title)
